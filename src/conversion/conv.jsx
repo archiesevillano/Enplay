a9e0a3f28a4cafa62b9933a61d992e8426bffeab
archiesevillano <archie.sevillano29@gmail.com>
2023-07-09 15:14:34 +0800

    Added Contents in file conv.jsx

--- src/conversion/conv.jsx
+++ src/conversion/conv.jsx
@@ -1,6 +1,11 @@
+import URLInput from "../components/URLInput/URLInput";
+
 const Conversion = () => {
     return (
-        <h1>Conversion</h1>
+        <div className="container">
+            <URLInput />
+            <p className="col col-md-7" style={{ fontFamily: "var(--inter)", fontSize: "0.7rem", transform: "translate(0, -13px)" }}>Search and Copy the URL of the video you wish to download and Paste it in here</p>
+        </div>
     );
 }
 
