f39b1c3b5488c9dcc148eaf73a1c7a86a83ea352
archiesevillano <archie.sevillano29@gmail.com>
2023-07-11 20:57:15 +0800

    Added a link to brand component

--- src/components/Brand/Brand.jsx
+++ src/components/Brand/Brand.jsx
@@ -1,13 +1,17 @@
 import "./Brand.css";
+import { Link } from 'react-router-dom';
+
 const Brand = () => {
     return (
-        <div className="brand">
-            <h1 className="brand__text">
-                <span className="d-inline-block brand__text__en">{`${import.meta.env.VITE_APP_TITLE}`.slice(0, 2)}</span>
-                <span className="brand__text__play">{`${import.meta.env.VITE_APP_TITLE}`.slice(2)}</span>
-                <span className="brand__version">{import.meta.env.VITE_APP_VERSION}</span>
-            </h1>
-        </div>
+        <Link to="/" className="brandLink">
+            <div className="brand">
+                <h1 className="brand__text">
+                    <span className="d-inline-block brand__text__en">{`${import.meta.env.VITE_APP_TITLE}`.slice(0, 2)}</span>
+                    <span className="brand__text__play">{`${import.meta.env.VITE_APP_TITLE}`.slice(2)}</span>
+                    <span className="brand__version">{import.meta.env.VITE_APP_VERSION}</span>
+                </h1>
+            </div>
+        </Link>
     );
 };
 
