b3462344930963fa34febdc68fae4581efe6f91c
archiesevillano <archie.sevillano29@gmail.com>
2023-07-12 15:55:03 +0800

    Added image logo in Brand Component

--- src/components/Brand/Brand.jsx
+++ src/components/Brand/Brand.jsx
@@ -1,13 +1,14 @@
 import "./Brand.css";
 import { Link } from 'react-router-dom';
+import logo from "./../../../EnplayLogo.svg";
 
 const Brand = ({ scale }) => {
     return (
         <Link to="/" className="brandLink">
             <div className="brand" style={{ transform: `scale(${scale === undefined ? 1 : scale})` }}>
+                <img className="brand__logo" src={logo} />
                 <h1 className="brand__text">
-                    <span className="d-inline-block brand__text__en">{`${import.meta.env.VITE_APP_TITLE}`.slice(0, 2)}</span>
-                    <span className="brand__text__play">{`${import.meta.env.VITE_APP_TITLE}`.slice(2)}</span>
+                    <span className="brand__text__play">{import.meta.env.VITE_APP_TITLE}</span>
                     <span className="brand__version">{import.meta.env.VITE_APP_VERSION}</span>
                 </h1>
             </div>
